666ce2bdf928a20e8e1469aa02a71ba7d58e3130
kamilkow1123 <22kamil.kow@gmail.com>
2021-08-07 04:03:32 +0200

    Cheatsheet of the basic setup for JEST tests

---
+++ functions.test.js
@@ -0,0 +1,73 @@
+const functions = require("./functions");
+
+// toBe
+test("Adds 2 + 2 to equal 4", () => {
+    expect(functions.add(2, 2)).toBe(4);
+});
+
+// not
+test("Adds 2 + 2 to NOT equal 5", () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// toBeNull
+test("Should be null", () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test("Should be falsy", () => {
+    expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toEqual
+test("User should be Kamil Kowalewski object", () => {
+    expect(functions.createUser()).toEqual({
+        firstName : "Kamil",
+        lastName  : "Kowalewski",
+    });
+});
+
+// toBeLessThan and toBeGreaterThan
+test("Should be under 1600", () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+});
+
+// Regex
+test("There is no I in team", () => {
+    expect("team").not.toMatch(/I/);
+});
+
+// Arrays
+test("Admin should be in usernames", () => {
+    usernames = [ "john", "paul", "admin" ];
+
+    expect(usernames).toContain("admin");
+});
+
+// Working with async data
+
+// Promise
+test("User fetched name should be Leanne Graham", () => {
+    expect.assertions(1);
+    return functions.fetchUser().then(data => {
+        expect(data.name).toEqual("Leanne Graham");
+    });
+});
+
+// Async Await
+test("User fetched name should be Leanne Graham", async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+
+    expect(data.name).toEqual("Leanne Graham");
+});
